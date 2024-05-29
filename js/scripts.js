adfed2695469233433bc86804d51b77d99c11b18
Daniel Contrino <daniel@coheda.com>
2024-05-29 16:40:50 +0000

    Correcciones en el menú

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
+/**
+ * En esta función para controlar el menú, primero recorremos el DOM buscando todos los botones (.menu-item-expand)
+ * Luego, en cada uno, escuchamos el evento Click.
+ * 
+ * En el evento, buscamos el sub-menú que se encuentre dentro del menú donde hicimos click (.menu-item)
+ * Si el sub-menú existe, agreguamos o quitamos la clase hidden según corresponda.
+ * 
+ */
+
 document.querySelectorAll(".menu-item-expand").forEach(element => {
     element.addEventListener("click", event => {
-        const submenu = event.target.closest(".menu").querySelector(".menu");
+        const submenu = event.target.closest(".menu-item").querySelector(".menu");
         if (submenu) submenu.classList.toggle("hidden");
     });
 });
